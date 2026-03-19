fecfe2ca0c1f4e93c93603d54ba4de4d54e88a33
Michael Freeman <mfreeman451@gmail.com>
2026-03-18 20:57:39 -0500

    chore: release 0.8.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Phoenix.ReactServer.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/mfreeman451/phoenix-react"
-  @version "0.8.0"
+  @version "0.8.1"
 
   def project do
     [
